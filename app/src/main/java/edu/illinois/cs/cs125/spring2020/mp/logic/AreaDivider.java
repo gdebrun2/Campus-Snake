45b2da2ed83962af71121e27af78b7c81018081c
gdebrun2 <gdebrun2@illinois.edu>
2020-02-18 18:29:39 -0600

    WithinRange, TargetVisited, TargetCandidate, SnakeRule, linecrossdetector, checkstyle, VisitTarget, PlaceMarker onLocationUpdate MP1 AreaDivider

---
+++ app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/AreaDivider.java
@@ -0,0 +1,17 @@
+package edu.illinois.cs.cs125.spring2020.mp.logic;
+
+public class AreaDivider {
+    public AreaDivider(double setNorth,
+                       double setEast,
+                       double setSouth,
+                       double setWest,
+                       int setCellSize) {
+        public boolean isValid() {
+
+        }
+    }
+    public getCellBounds(int x, int y) {
+
+    }
+
+}
